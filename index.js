38d90232241420a5306d31ab0263dbfb72ca838c
FedericoPalaz <federico.palazzini@studenti.unitn.it>
2016-11-30 22:16:24 +0100

    Modificato la comunicazione con il server

--- index.js
+++ index.js
@@ -17,12 +17,16 @@ var pg = require('pg');
 //connessione al database
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM prova', function(err, result) {
       done();
       if (err)
-       { console.error(err); response.send("Error " + err); }
+       { console.error(err); 
+	 response.send("Error " + err); 
+	}
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { 
+	response.render('pages/db', {results: result.rows} ); 
+	}
     });
   });
 });
