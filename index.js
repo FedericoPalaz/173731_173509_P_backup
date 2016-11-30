942fc5117b4680604a12c38f473d8c1aea5f20d9
FedericoPalaz <federico.palazzini@studenti.unitn.it>
2016-11-30 17:16:01 +0100

    Aggiunto file index e modificato json

---
+++ index.js
@@ -0,0 +1,49 @@
+//express
+var express=require('express');
+
+//init express
+var app=express();
+
+//for templates
+var bind = require('bind');
+
+//for URL
+//var url = require('url');
+
+//pg
+var pg = require('pg');
+
+//connessione al database
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
+
+
+//for POST
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//listen in a specific port
+app.set('port', (process.env.PORT || 5000));
+
+//mount middlewear (allow to show static files)
+app.use('/public',express.static(__dirname+'/public'));
+
+app.get('/',function (req,res) {
+    
+        res.redirect('public/tpl/login.html');
+});
+
+//app start listening
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
