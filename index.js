300dd40ece98d7d96866b5b2764e9399e94a2328
FedericoPalaz <federico.palazzini@studenti.unitn.it>
2016-12-01 11:19:51 +0100

    modificato connessione db

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ var pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://dbSW:password@localhost:5432/dbSW';
 
 const client = new pg.Client(connectionString);
-client.connect();
+
 
 //connessione al database
 app.get('/db', function (request, response) {
+    client.connect();
   const query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
     query.on('end', () => { client.end(); });
 });
