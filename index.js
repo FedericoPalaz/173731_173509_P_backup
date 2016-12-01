37cc2a1d0233099be9f9ccf8e32b2507d92728b2
zihadulazam <zihadul.azam@studenti.unitn.it>
2016-12-01 14:00:56 +0100

    local changes

--- index.js
+++ index.js
@@ -11,26 +11,6 @@ var bind = require('bind');
 //var url = require('url');
 
 
-//pg
-var pg = require('pg');
-
-//connessione al database
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM prova', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); 
-	 response.send("Error " + err); 
-	}
-      else
-       { 
-	response.render('pages/db', {results: result.rows} ); 
-	}
-    });
-  });
-});
-
 
 
 
