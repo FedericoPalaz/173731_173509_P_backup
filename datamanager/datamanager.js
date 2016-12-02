46c483dba77776307bb3c5827cb498106eca6e29
zihadulazam <zihadul.azam@studenti.unitn.it>
2016-12-02 11:15:28 +0100

    aggiunto /login

--- datamanager/datamanager.js
+++ datamanager/datamanager.js
@@ -1,52 +1,15 @@
 //express
 var express=require('express');
 
-//init express
-var app=express();
-
 //for templates
 var bind = require('bind');
-
-//manages sessions
-var session = require('express-session')
-
-//include dataBase Manager
-var datamanager=require('./datamanager/datamanager.js');
 
 //pg
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://dbSW:password@localhost:5432/dbSW';
 
-//use sessions
-/*
-app.use(session({ 
-	//required, used to prevent tampering
-	secret: 'string for the hash', 
-	//set time of validity of cookies
-	cookie: { maxAge: 60000 }
-}));
-*/
 
-
-
-//for POST
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//listen in a specific port
-app.set('port', (process.env.PORT || 5000));
-
-//mount middlewear (allow to show static files)
-app.use('/public',express.static(__dirname+'/public'));
-
-
-app.use('/', function (req,res) {
-    res.statusCode = 200; 
-    res.redirect('public/tpl/login.html');
-});
-
-app.post('/login', function(req,res) 
-{
+function login(req,res) {
     console.log('entrato');
     if(typeof req.body!='undefined' && req.body){
         var email=req.body.email;
@@ -76,35 +39,7 @@ app.post('/login', function(req,res)
             });
         }
     }
-});
+    
+}
 
-
-
-/**
- * @brief log out page
- * @return a page with notification that user is logged out, or a page which says that the user is already logged out.
- */
-app.get('/logout', function(request, response) 
-{
-	var text = "";
-	
-	//check if the session exists
-	if (request.session.user_id !=null) 
-	{    	
-		request.session.user_id = null;
-  	}
-	else
-	{
-		text = 'You are already logged out';
-	}
-	
-	//write response
-    res.redirect('public/tpl/login.html');
-});
-
-//app start listening
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-
-});
-
+exports.login=login;
